3985edb7b1774e8b735b11ea89d26486cd66ba71
Quang Long <trinhq011.com>
2023-03-23 11:00:46 +0700

    git clone https://github.com/Souutaa/DCT1205.git

--- DCT1205/Models/DetailEmployeeViewModel.cs
+++ DCT1205/Models/DetailEmployeeViewModel.cs
@@ -1,39 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DCT1205.Entity;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DCT1205.Entity
+namespace DCT1205.Models
 {
-    public class Employee
+    public class DetailEmployeeViewModel
     {
-        [Key]
         public int Id { get; set; }
-        [Required, MaxLength(50)]
-        public string FirstName { get; set; }
-        public string? MiddleName { get; set; }
-        [Required, MaxLength(50)]
-        public string LastName { get; set; }
-        public string FullName { get; set; }
         public string EmployeeNo { get; set; }
+        public string FullName { get; set; }
         public string Gender { get; set; }
         public string ImageUrl { get; set; }
         public DateTime DOB { get; set; }
         public DateTime DateJoined { get; set; }
+        public string Phone { get; set; }
         public string Designation { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
         public string NationalInsuranceNo { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
         public StudentLoan StudentLoan { get; set; }
         public UnionMember UnionMember { get; set; }
-        [Required, MaxLength(150)]
         public string Address { get; set; }
-        [Required, MaxLength(50)]
         public string City { get; set; }
-        [Required, MaxLength(50)]
         public string Postcode { get; set; }
     }
 }
